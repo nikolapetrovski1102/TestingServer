92db9d62b922b306f40f7f604356e8f9e48e3dd0
Nikola Petrovski <nikola_petrovski02@outlook.com>
2022-03-24 01:04:08 +0100

    scroll reveal

---
+++ assets/js/ScrollReveal.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '200px',
+    rotate: {x: 15, y: 15, z: 15},
+    duration: 1000,
+    reset: false
+});
+
+sr.reveal("#work span", {
+    interval: 200
+})
